8023ab71de5c86fe69616cf1aaa376552e5ad3c2
ghanshyam <ghanshyam.sikri@gmail.com>
2021-09-08 17:57:15 +0530

    super hero added loading animation

--- superhero/README.md
+++ superhero/README.md
@@ -1,45 +1,50 @@
 # search SuperHero and make them favorite
-before going ahead visit the [Demo](https://wizzenalum1.github.io/superhero) 
+
+before going ahead visit the [Demo](https://wizzenalum1.github.io/superhero)
+
 > To recreate this you can visit my [youtube](wizzenalum.com).
-> Recreation will need vanila javascript, html, css only also need to create the token number to use superhero Api.
-## Folder structure 
+> Recreation will need vanila javascript, html, css only also need to create the token number to use [superhero-Api](https://www.superheroapi.com/).
+
+## Folder structure
+
 It has minimilistic folder structure with entry point index.html.
->├── README.md
->├── index.html
->├── script
->│   ├── Component.js
->│   └── index.js
->└── style
->    └── style.css
+
+> ├── README.md \
+> ├── index.html \
+> ├── script \
+> │ ├── Component.js \
+> │ └── index.js \
+> └── style \
+> └── style.css \
 
 ## File wise code
+
 actually index.html only has metadata and links to connect to css and script. so all the html will be created during the runing the web application using javascript.
 
 1. render() will render the UI as the states changes by any events and most of code for rendering the deferrent views are in the Component.js. there is three views home, favorite and hero.
 2. index.js has states that decides the current view by currentWindow variable and other data shown in the page by defferent other variables of the object state.
 3. index.js also hase all the event listeners with eventlistner controller (manageEvents) that set listners according to the current view.
-4. index.js also manage the api call as user search. 
+4. index.js also manage the api call as user search.
 5. css is minimilist but enough to make presentable.
 
 ## Code flow
+
 here i am showing how all function call going to happen with the user intrect with the GUI.
-- What happen each time 
+
+- What happen each time
+
 ```sh
 index.html--> render()-->rootComponent() it call view specific function --> manageListenrs() it set listeners according to the view.
 ```
+
 - initial loading of the web application
+
 ```sh
 render()--> rootComponent(state)--> setSearchComponent(state)--> createList(list,home)--> manageListenrs()--> rootListeners()-->searchlistners()
 ```
+
 - when user click to search for superhero
+
 ```sh
 handleSearchClick()--> handleXMLRequest()--> here new state is set --> render()--> rootComponent(state)--> setSearchComponent(state)--> createList(list,home)--> manageListenrs()--> rootListeners()-->searchlistners()
 ```
-
-
-
-
-
-
-
-
--- superhero/script/Component.js
+++ superhero/script/Component.js
@@ -1,15 +1,15 @@
-// here all the html that will be load at the time of the   
+// here all the html that will be load at the time of the
 
 // this function will create list of hero.cards that will be used to show the search list
 // or favorite list.
-let createList = function(list,home){
-    if(!list) return false;
-    let htmlComponent = "";
-    let favText = home==="home"?"Favorite":"UnFavorite"
-    let favColor = home==="home"?"":"red"
-    for(let i = 0;i<list.length;i++){
-        let hero = list[i];
-        htmlComponent+=`
+let createList = function (list, home) {
+  if (!list) return false;
+  let htmlComponent = "";
+  let favText = home === "home" ? "Favorite" : "UnFavorite";
+  let favColor = home === "home" ? "" : "red";
+  for (let i = 0; i < list.length; i++) {
+    let hero = list[i];
+    htmlComponent += `
         <div class = "hero-card"> 
         
                 <img src=${hero.image.url} alt="IMAGE">
@@ -20,17 +20,22 @@ let createList = function(list,home){
                 <div id = "fav-${i}" style = "background-color:"blue" class ="hero-btn btn">more...</div>
             </div>
         </div>
-        `
-    }
-    return htmlComponent;
-}
+        `;
+  }
+  return htmlComponent;
+};
 
 // this create the main page of the search page with the help of createlist function
-let setSearchComponent = function(state){
-    console.log("SEARCH")
-    let list = createList(state.searchList,state.currentWindow);
-    if(!list) list = `<h2 style="margin:auto">type any SuperHero name</h2>`;
-    let htmlText = `
+let setSearchComponent = function (state) {
+  console.log("SEARCH", state.searchList);
+  let list = "";
+  if (state.searchList.constructor === String) {
+    list = `<h2> ${state.searchList}</h2>`;
+  } else {
+    list = createList(state.searchList, state.currentWindow);
+  }
+  console.log("fromt you", list);
+  let htmlText = `
             <main>
                 <div class="search">
                     <input type="text" id="search-input"><div id="search-btn" class = "btn">search</div>
@@ -39,48 +44,59 @@ let setSearchComponent = function(state){
 
             </main>
    `;
-   return htmlText ;
-}
+  return htmlText;
+};
 
 // this function will return the main page of the favorite.
-let setFavoriteComponent = function(state){
-    console.log("FAVORITE");
-    let list = createList(state.favoriteHeroList,state.currentWindow);
-    if(!list){
-        list = `<h2>Your favorite list is Empty</h2>`;
-    }
+let setFavoriteComponent = function (state) {
+  console.log("FAVORITE");
+  let list = createList(state.favoriteHeroList, state.currentWindow);
+  if (!list) {
+    list = `<h2>Your favorite list is Empty</h2>`;
+  }
 
-    let htmlText = `<main>${list}</main>`;
-    return htmlText;
-}
-
+  let htmlText = `<main>${list}</main>`;
+  return htmlText;
+};
 
 // this will create main page of hero section.
-let setHeroComponent = function({hero}){
-    let appearance = "",biography = '',powerstats="",relations="";
-    for(let property in hero.appearance){
-        if(hero.appearance[property].constructor===String && hero.appearance[property].length>2) {
-            appearance +=`<li>${property} : ${hero.appearance[property]}</li>`
-        }else if(parseInt(hero.appearance[property][1][0])){
-            appearance +=`<li>${property} : ${hero.appearance[property][1]}</li>`           
-        }
+let setHeroComponent = function ({ hero }) {
+  let appearance = "",
+    biography = "",
+    powerstats = "",
+    relations = "";
+  for (let property in hero.appearance) {
+    if (
+      hero.appearance[property].constructor === String &&
+      hero.appearance[property].length > 2
+    ) {
+      appearance += `<li>${property} : ${hero.appearance[property]}</li>`;
+    } else if (parseInt(hero.appearance[property][1][0])) {
+      appearance += `<li>${property} : ${hero.appearance[property][1]}</li>`;
     }
-    for(let property in hero.biography){
-        if(hero.biography[property].constructor===String && hero.biography[property].length>2) {
-            biography +=`<li>${property} : ${hero.biography[property]}</li>`
-        }
+  }
+  for (let property in hero.biography) {
+    if (
+      hero.biography[property].constructor === String &&
+      hero.biography[property].length > 2
+    ) {
+      biography += `<li>${property} : ${hero.biography[property]}</li>`;
     }
-    for(let property in hero.relations){
-        if(hero.connections[property].constructor===String && hero.connections[property].length>2) {
-            connections +=`<li>${property} : ${hero.connections[property]}</li>`
-        }
-    }  
-    for(let property in hero.powerstats){
-        if(parseInt(hero.powerstats[property])) {
-            powerstats +=`<li>${property} : ${hero.powerstats[property]}</li>`
-        }
-    }  
-    return `<main class="page">
+  }
+  for (let property in hero.relations) {
+    if (
+      hero.connections[property].constructor === String &&
+      hero.connections[property].length > 2
+    ) {
+      connections += `<li>${property} : ${hero.connections[property]}</li>`;
+    }
+  }
+  for (let property in hero.powerstats) {
+    if (parseInt(hero.powerstats[property])) {
+      powerstats += `<li>${property} : ${hero.powerstats[property]}</li>`;
+    }
+  }
+  return `<main class="page">
     <header>
             <img src="${hero.image.url}" alt="No Image found">
             <h1>${hero.name}</h1>
@@ -104,31 +120,30 @@ let setHeroComponent = function({hero}){
        ${relations}
    </details>
    </main>`;
-}
-
+};
 
 // this function is main controller which call the defferent page according to the currentwindow sected.
 
-export let rootComponent = function(state){
-    let currentWindow = state.currentWindow;
-    let homeColor="",favColor = "";
-    console.log("RENDER ")
-    let callback = setHeroComponent;
-    if(currentWindow === "home"){
-        callback = setSearchComponent;
-        homeColor= "salmon"
-    }
-    if(currentWindow === "favorite"){
-        callback = setFavoriteComponent;
-        favColor = "salmon";
-    }
-    if(currentWindow==="hero") callback = setHeroComponent
-    return `
+export let rootComponent = function (state) {
+  let currentWindow = state.currentWindow;
+  let homeColor = "",
+    favColor = "";
+  console.log("RENDER ");
+  let callback = setHeroComponent;
+  if (currentWindow === "home") {
+    callback = setSearchComponent;
+    homeColor = "salmon";
+  }
+  if (currentWindow === "favorite") {
+    callback = setFavoriteComponent;
+    favColor = "salmon";
+  }
+  if (currentWindow === "hero") callback = setHeroComponent;
+  return `
             <header class="nav">
                 <div id="home" style = "background-color:${homeColor}" class="btn">HOME</div>
                 <div id="favorite" style = "background-color:${favColor}"  class = "btn">Favorite</div>
             </header>
             ${callback(state)}
-        `
-   
-}
+        `;
+};
